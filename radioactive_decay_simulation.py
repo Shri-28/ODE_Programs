635dcfde6c9b190ee25be03ab8415d15e72bd5ea
Shri-28 <39884509+Shri-28@users.noreply.github.com>
2025-10-25 23:09:02 +0200

    Created program : radioactive_decay_simulation.py

---
+++ radioactive_decay_simulation.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import odeint
+
+# y' = -a*y
+def decay(y, t, a):
+    return -a * y
+
+a = float(input("Enter the decay constant :"))         # decay constant
+y0 = int(input("Enter the initial amount :")         # initial amount
+t = np.linspace(0, 10, 100)  # time from 0 to 10
+
+solution = odeint(decay, y0, t, args=(a,))
+
+plt.plot(t, solution, label=f'Decay (a={a})')
+plt.xlabel('Time')
+plt.ylabel('Amount y(t)')
+plt.title('Radioactive Decay y\' = -a*y')
+plt.legend()
+plt.grid(True)
+plt.show()
